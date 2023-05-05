3d1c5b99f986efddd54104af1dc2c094dd328d5a
KarimB101 <karim.baptiste@yahoo.com>
2023-05-05 11:54:54 -0700

    edit seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO department (name)
 VALUES 
-    ("dep 1"),
-    ("dep 2"),
-    ("dep 3");
+    ("sales"),
+    ("repair"),
+    ("managment");
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-    ("role 1", 10000.00, 1),
-    ("role 2", 20000.00, 2),
-    ("role 3", 30000.00, 3);
+    ("salesman", 10000.00, 1),
+    ("engineer", 20000.00, 2),
+    ("manager", 30000.00, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("emp", "a", 1, null),
-    ("emp", "b", 2, 1),
-    ("emp", "c", 3, 1);
+    ("jon", "doe", 1, null),
+    ("ben", "smith", 2, 1),
+    ("jack", "brown", 3, 1);
