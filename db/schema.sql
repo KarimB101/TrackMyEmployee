8e7e0febfd14fef477d72e2fb9dbdeca26f81ce8
KarimB101 <karim.baptiste@yahoo.com>
2023-05-05 17:52:47 -0700

    edit db

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL, 
     salary DECIMAL NOT NULL,
-    department_id INT, 
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    departments_id INT, 
+    FOREIGN KEY (departments_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    roles_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
     manager_id INT NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,16 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES 
     ("sales"),
     ("repair"),
     ("managment");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, departments_id)
 VALUES 
     ("salesman", 10000.00, 1),
     ("engineer", 20000.00, 2),
     ("manager", 30000.00, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES
     ("jon", "doe", 1, 1),
     ("ben", "smith", 2, 1),
